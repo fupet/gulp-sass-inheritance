1f1d1c1608b100c5e54a18220c35e53068886ce5
Peter Furst <peter.furst@doclerholding.com>
2015-11-04 13:35:15 +0100

    multi-level dependency resolve

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var es = require('event-stream');
-var _ = require('lodash');
-var fs = require('fs');
+var _ = require("lodash");
+var vfs = require('vinyl-fs');
+var through2 = require('through2');
 var gutil = require('gulp-util');
 var sassGraph = require('sass-graph');
 var PLUGIN_NAME = 'gulp-sass-inheritance';
@@ -16,49 +17,54 @@ function gulpSassInheritance(options) {
 
   function writeStream(currentFile) {
     if (currentFile && currentFile.contents.length) {
-      files.push(currentFile);
+      files.push(currentFile.path);
     }
   }
 
-  function endStream() {
-    var stream = this;
-    if (files.length) {
-      var allPaths = _.pluck(files, 'path');
-      var graph = sassGraph.parseDir(options.dir, options);
-      var newFiles = files;
-      _.forEach(files, function(file) {
-        if (graph.index && graph.index[file.path]) {
-          var fullpaths = graph.index[file.path].importedBy;
+  function findWhoImports(files, graph) {
+    return _.flatten(_.map(files, function(file) {
 
-          fullpaths.forEach(function (path) {
-            if (!_.include(allPaths, path)) {
-              allPaths.push(path);
-              newFiles.push(new gutil.File({
-                cwd: file.cwd,
-                base: file.base,
-                path: path,
-                stat: fs.statSync(path),
-                contents: fs.readFileSync(path)
-              }));
-            }
+      if (graph.index && graph.index[file]) {
+        var fullpaths = graph.index[file].importedBy;
+        if (fullpaths.length) {
+          var needsToResolve = _.filter(fullpaths, function(file) {
+            return /\/_/.test(file)
           });
 
-          if (options.debug) {
-            console.log('File', file.path);
-            console.log(' - importedBy', fullpaths);
+          if (needsToResolve.length) {
+            fullpaths = _.difference(fullpaths, needsToResolve);
+            fullpaths = _.union(fullpaths, findWhoImports(needsToResolve, graph));
           }
         }
+        else if (!/\/_/.test(file)) {
+          fullpaths.push(file);
+        }
 
-      });
-      es.readArray(files)
-        .pipe(es.through(
-          function (f) {
-            stream.emit('data', f);
-          },
-          function () {
-            stream.emit('end');
-          }
-      ));
+        if (options.debug) {
+          console.log('File', file);
+          console.log(' - importedBy', fullpaths);
+        }
+        return fullpaths;
+      }
+      return [];
+    }));
+  }
+
+  function endStream() {
+    if (files.length) {
+      var graph = sassGraph.parseDir(options.dir, options);
+
+      var filesPaths = _.unique(findWhoImports(files, graph));
+
+      vfs.src(filesPaths)
+          .pipe(es.through(
+              function (f) {
+                stream.emit('data', f);
+              },
+              function () {
+                stream.emit('end');
+              }
+          ));
     } else {
       stream.emit('end');
     }
